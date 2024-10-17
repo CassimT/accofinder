b9552b47d97f3f6179dd2d64af86d506fc653af1
CassimT <bed-com-24-22@unima.ac.mw>
2024-10-17 21:42:27 +0200

    Modulized routing

--- accofinder/src/App.js
+++ accofinder/src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import HomPage from "./pages/HomPage";
-import AgentPage from "./pages/AgentPage";
-import DetailsPage from "./pages/DetailsPage";
-import SingInPage from "./pages/SingInPage";
-import SingUpPage from "./pages/SingUpPage";
-import PaymentPage from "./pages/PaymentPage";
+import MainLayout from "./MainLayout";
+
 
 function App() {
   return (
-    <Router>
       <div>
-        <Routes>
-          <Route path="/" element={<HomPage />} />         
-          <Route path="/agents" element={<AgentPage />} />
-          <Route path="/details" element={<DetailsPage />} />
-          <Route path="/signin" element={<SingInPage />} />
-          <Route path="/signup" element={<SingUpPage />} />
-          <Route path="/payment" element={<PaymentPage />} />
-        </Routes>
+        <MainLayout/>
       </div>
-    </Router>
+     
   );
 }
 

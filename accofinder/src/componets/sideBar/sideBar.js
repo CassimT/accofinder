fe092c2e9119ff59045ec39d7498c267a764f024
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-10-18 20:24:02 +0200

    my first commit to my branch

---
+++ accofinder/src/componets/sideBar/sideBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SideBar(){
+  return (
+    <div className='flex'>
+        <div className='w-72 h-screen bg-purple'>side bar</div>
+        <div className='p-7 text2xl font-semibold flex-1 h-screen'>
+            <h1>home page</h1>
+
+        </div> 
+      
+    </div>
+  )
+}
--- accofinder/src/pages/DetailsPage.js
+++ accofinder/src/pages/DetailsPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import SideBar from '../componets/sideBar/SideBar'
 
 export default function DetailsPage() {
   return (
-    <div>DetailsPage</div>
+    <div>
+      <SideBar/>
+    </div>
   )
 }

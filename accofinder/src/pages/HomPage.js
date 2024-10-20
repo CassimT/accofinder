390660346c62902b8e619d2bb7a40a1126b47f81
CassimT <bed-com-24-22@unima.ac.mw>
2024-10-20 17:49:18 +0200

    resolving conflicts

--- accofinder/src/pages/HomPage.js
+++ accofinder/src/pages/HomPage.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import Listing from '../componets/home/recent/Listing';
-=======
 import React from 'react'
 import Listing from '../componets/home/recent/Listing'
 import Heading from '../componets/home/recent/Heading'
->>>>>>> 65ec078f2bbb47bc0b8828684d12546d69ef6e72
 
 export default function HomPage() {
   return (

5d9723f70b0be2d99633472a29a1b67a2e471868
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-10-22 20:45:14 +0200

    created a detailscard

---
+++ accofinder/src/componets/details/DetailsCard.js
@@ -0,0 +1,13 @@
+import { GiPathDistance } from "react-icons/gi";
+
+export const listdetailsdata= [
+    {
+      icons: <GiPathDistance/>,
+      location: "Family House",
+      distance :"2.8 km",
+      price :"MK 25,000",
+      security:" not available",
+    }
+]
+/*created an object for the listing details */
+    

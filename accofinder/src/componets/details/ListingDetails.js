bc00ea75a4e71b0a7feea400fb16321ab08a4d15
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-10-23 22:40:03 +0200

    edited listingDetails

--- accofinder/src/componets/details/ListingDetails.js
+++ accofinder/src/componets/details/ListingDetails.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
 
-function ListingDetails({ title, subtitle, children }) {
+function ListingDetails({ icons, location , distance,price,security }) {
   return (
     <div className="bg-blue-900 h-72 w-60 rounded-md flex flex-col p-4 font-roboto">
-      <h2 className="text-white text-lg font-bold">{title}</h2>
-      <p className="text-white text-sm">{subtitle}  </p>
-      <div className="flex-1 overflow-y-auto">{children}</div>
+      <h2 className="text-white text-lg font-roboto"></h2>
+      <p className="text-white text-sm">  </p>
+      <div className="flex-1 overflow-y-auto"></div>
     </div>
   );
 }

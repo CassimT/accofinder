ad727c57ad7d2372cc7d9541142301db6e117287
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-10-24 19:52:43 +0200

    added some files

--- accofinder/src/componets/details/ListingDetails.js
+++ accofinder/src/componets/details/ListingDetails.js
@@ -4,7 +4,7 @@ import React from 'react';
 function ListingDetails({ icons, location , distance,price,security }) {
   return (
     <div className="bg-blue-900 h-72 w-60 rounded-md flex flex-col p-4 font-roboto">
-      <h2 className="text-white text-lg font-roboto"></h2>
+      <h1 className="text-white text-lg font-roboto"></h1>
       <p className="text-white text-sm">  </p>
       <div className="flex-1 overflow-y-auto"></div>
     </div>

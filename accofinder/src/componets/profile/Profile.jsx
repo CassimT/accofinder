9ac985003020738bdba5bf19d0c8a776fd1c7e4f
Benadetta Damiano <bsc-inf-29-22@unima.ac.mw>
2024-10-30 14:24:43 -0700

    Creat profile component

--- accofinder/src/componets/profile/Profile.jsx
+++ accofinder/src/componets/profile/Profile.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div> 
+        <div className=' bg-blue-400 w-screen h-96 flex flex-col gap-16 justify-center relative items-center'>
+            <div className=' bg-white w-72 h-72 rounded-full -mb-80 absolute'>
+               
+            </div>
+            <h className='text-black text-2xl font-bold'>STUDENT</h>
+                
+        </div>
+          <p className='text-center mt-4'>Abram prosser </p>
+          <p className='text-center mt-4'>HelgaR.Kovanch@exam.com </p>
+          
+          <button className="text-white bg-black px-6 py-3 h-10 flex justify-center items-center
+           rounded-full cursor-pointer mx-auto mt-6">LogOut</button>
+          
+        </div> 
+   )
 }

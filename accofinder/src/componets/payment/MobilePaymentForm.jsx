f75975fe57897d195259afcd8e70b59f3192b618
Sydney Mtima <bsc-com-22-22@unima.ac.mw>
2024-11-01 19:54:20 +0200

    changed file names and also modified files content

--- accofinder/src/componets/payment/MobilePaymentForm.jsx
+++ accofinder/src/componets/payment/MobilePaymentForm.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { useState} from "react";
-
+import {useForm} from "react-hook-form";
 
 const MobilePaymentForm = () =>{
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
     
-  const [username, setUsername] = useState("");
-  const [phone, setPhone] = useState("");
-  const [amount, setAmount] = useState("");
-
   // function for handling form submition
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ username, phone, amount });
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
     return(
@@ -25,7 +24,7 @@ const MobilePaymentForm = () =>{
         {/* payment form for mobile wallet */}
         <div className="border border-gray-50 bg-[#f4f4f4] shadow-xl w-[40%] h-[60vh] rounded-xl absolute -mb-96">
           <form
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col gap-y-4 items-center justify-center mt-8"
           >
             <h3 className="font-semibold text-xl mb-3">Mobile payment</h3>
@@ -33,35 +32,35 @@ const MobilePaymentForm = () =>{
               Username
               <input
                 type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                name = "username"
+                {...register("username", { required: true })}
                 placeholder="Cesar Medrano"
-                className="py-2 px-8 rounded-lg border-2 border-gray-200 bg-gray-100"
+                className="py-2 px-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 border-2 border-gray-200 bg-gray-100"
               />
             </label>
             <label className="flex flex-col font-sm">
               Phone number
               <input
                 type="phone"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
+                name="phoneNumber"
                 placeholder="+265"
-                className="py-2 px-8  rounded-lg border-2 border-gray-200 bg-gray-100"
+                {...register("phoneNumber", { required: true })}
+                className="py-2 px-8 focus:outline-none focus:ring-2 focus:ring-orange-500  rounded-lg border-2 border-gray-200 bg-gray-100"
               />
             </label>
             <label className="flex flex-col font-sm">
               Amount
               <input
                 type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
+                name="amount"
+                {...register("amount", { required: true })}
                 placeholder="MK0.00"
-                className="py-2 px-8 rounded-lg border-2 border-gray-200 bg-gray-100"
+                className="py-2 px-8 focus:outline-none focus:ring-2 focus:ring-orange-500 rounded-lg border-2 border-gray-200 bg-gray-100"
               />
             </label>
             <button
               type="submit"
-              className="px-8 text-bold py-2 mt-5 rounded-xl bg-black text-white hover:scale-95 tranition duration-500"
+              className="px-10 text-bold py-2 mt-5 rounded-xl bg-black text-white hover:scale-95 tranition duration-500"
             >
               Pay
             </button>
--- accofinder/src/componets/payment/PaymentSelection.js
+++ accofinder/src/componets/payment/PaymentSelection.js
@@ -1,11 +1,11 @@
 import { React } from "react";
 import { FaCreditCard } from "react-icons/fa";
 
-const Payments = () => {
+function PaymentSelection(){
  
   return (
     <div className="w-full h-screen">
-      <div className="flex flex-col h-[60vh] items-center justify-center relative bg-[url('./pay3.jpg')] bg-cover bg-no-repeat">
+      <div className="flex flex-col h-[60vh] items-center justify-center relative bg-[url('./components/assets/pay3.jpg')] bg-cover bg-no-repeat">
         <p className="text-2xl font-semibold font-sans mb-14 text-center">
           Your payment is safe with us
           <br />
@@ -14,13 +14,11 @@ const Payments = () => {
 
         {/* card for selecting payment method */}
         <div className="flex flex-col items-center justify-center border border-gray-50 bg-[#f4f4f4] shadow-xl w-[40%] h-[55vh] rounded-xl absolute -mb-96">
-          {/* button for payment via credit caard */}
-          <button className="flex flex-row gap-4 hover:scale-95 transition duration-500 px-9 py-2 border-2 bg-indigo-200 border-indigo-400 rounded-lg font-sans font-semibold">
+          <button className="flex flex-row w-[70%] gap-4 hover:scale-95 transition duration-500 px-9 py-2 border-2 bg-indigo-200 border-indigo-400 rounded-lg font-sans font-semibold">
             <FaCreditCard className="mt-1" /> Pay via credit card
           </button>
-          
-          {/* button for mobile payment */}
-         <button className="hover:scale-95 transition duration-500 px-10 py-2 border-2 bg-indigo-200 border-indigo-400 rounded-lg font-sans font-semibold">
+        <p className="m-3">OR</p>
+         <button className="hover:scale-95 w-[70%] transition duration-500 px-10 py-2 border-2 bg-indigo-200 border-indigo-400 rounded-lg font-sans font-semibold">
             Mobile /Airtelmoney
           </button>
         </div>
@@ -28,4 +26,4 @@ const Payments = () => {
     </div>
   );
 };
-export default Payments;
+export default PaymentSelection;

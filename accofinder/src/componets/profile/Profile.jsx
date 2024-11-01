b98adb609b51fec9114e7b7f093d2c373a2e14d8
Benadetta Damiano <bsc-inf-29-22@unima.ac.mw>
2024-11-01 11:04:45 -0700

    change some contents

--- accofinder/src/componets/profile/Profile.jsx
+++ accofinder/src/componets/profile/Profile.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-export default function Profile() {
+import React from 'react';
+import { EditOutlined } from '@ant-design/icons';
+import img from '../profile/Profile';
+const Profile = () => {
   return (
-    <div> 
-        <div className=' bg-blue-400 w-screen h-96 flex flex-col gap-16 justify-center relative items-center'>
-            <div className=' bg-white w-72 h-72 rounded-full -mb-80 absolute'>
-               
-            </div>
-            <h className='text-black text-2xl font-bold'>STUDENT</h>
-                
+      <div className="w-80 bg-white rounded-lg shadow-lg overflow-hidden text-center relative">
+        <div className="bg-blue-200  text-blue-800 font-semibold py-1 text-lg absolute w-full h-28">
+          Student
         </div>
-          <p className='text-center mt-4'>Abram prosser </p>
-          <p className='text-center mt-4'>HelgaR.Kovanch@exam.com </p>
-          
-          <button className="text-white bg-black px-6 py-3 h-10 flex justify-center items-center
-           rounded-full cursor-pointer mx-auto mt-6">LogOut</button>
-          
-        </div> 
-   )
-}
+        <div className="flex justify-center bg-blue-200 mt-12 relative">
+          <div className="relative -mb-16">
+            <img
+              src="../logo512.png"
+              alt="Profile"
+              className="w-32 h-32 rounded-full border-2 border-blue-400 mx-auto"
+            />
+
+            <span className="absolute right-0 top-0 bg-blue-100 p-1 rounded-full">
+              <EditOutlined className="text-blue-500 text-lg" />
+            </span>
+          </div>
+        </div>
+
+        <div className="mt-20">
+          <h2 className="text-xl font-bold">Abram Prosser</h2>
+          <p className="text-gray-600">HelgaR.Kovach@example.com</p>
+        </div>
+
+        <div className="mt-6 mb-8">
+          <button className="px-4 py-2 bg-black text-white rounded-full hover:bg-gray-700">
+            Log Out
+          </button>
+        </div>
+      </div>
+
+  );
+};
+
+export default Profile;

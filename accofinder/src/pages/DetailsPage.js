193b68573064cb11a0546f17d7e0f13a76434026
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-11-03 12:46:45 +0200

    deleted some rendered links

--- accofinder/src/pages/DetailsPage.js
+++ accofinder/src/pages/DetailsPage.js
@@ -6,8 +6,7 @@ export default function DetailsPage() {
   return (
     <div className='flex bg-zinc-200 justify-center items-center flex-col min-h-screen w-screen'>
       <h1 className='text-amber-600 text-center font-roboto font-bold text-3xl pb-4 pt-4 '>More Details </h1>
- <Gallery/>
- <DetailsCard/>
+
     </div>
   )
 }

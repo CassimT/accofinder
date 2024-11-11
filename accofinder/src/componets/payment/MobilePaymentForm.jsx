c061df27f836279ec4b232eace93603cc150079d
Sydney Mtima <bsc-com-22-22@unima.ac.mw>
2024-11-11 22:29:06 +0200

    modified payment components and created button component

--- accofinder/src/componets/payment/MobilePaymentForm.jsx
+++ accofinder/src/componets/payment/MobilePaymentForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import Button from "./Button"
 
 const MobilePaymentForm = () =>{
   const {
@@ -15,7 +16,7 @@ const MobilePaymentForm = () =>{
 
     return(
     <div className="w-full h-screen">
-      <div className="bg-gradient-to-r to-blue-500 from-green-600 flex flex-col h-[60vh] items-center justify-center relative">
+      <div className="bg-gradient-to-r to-blue-500 from-green-600  flex flex-col h-[60vh] items-center justify-center relative">
         <p className="text-2xl font-semibold font-sans -mt-32 text-center">
           Your payment is safe with us <br /> Book with
           confidence
@@ -27,44 +28,39 @@ const MobilePaymentForm = () =>{
             onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col gap-y-4 items-center justify-center mt-8"
           >
-            <h3 className="font-semibold text-xl mb-3">Mobile payment</h3>
-            <label className="flex flex-col font-sm">
+            <h3 className="font-semibold text-xl  mb-3">Mobile payment</h3>
+            <label className="w-[60%] flex flex-col font-sm">
               Username
               <input
                 type="text"
                 name = "username"
                 {...register("username", { required: true })}
                 placeholder="Cesar Medrano"
-                className="py-2 px-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 border-2 border-gray-200 bg-gray-100"
+                className="py-2 px-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 border-2 border-gray-200 bg-gray-100"
               />
             </label>
-            <label className="flex flex-col font-sm">
+            <label className="flex flex-col w-[60%] font-sm">
               Phone number
               <input
                 type="phone"
                 name="phoneNumber"
                 placeholder="+265"
                 {...register("phoneNumber", { required: true })}
-                className="py-2 px-8 focus:outline-none focus:ring-2 focus:ring-orange-500  rounded-lg border-2 border-gray-200 bg-gray-100"
+                className="py-2 px-10 focus:outline-none focus:ring-2 focus:ring-orange-500  rounded-lg border-2 border-gray-200 bg-gray-100"
               />
             </label>
-            <label className="flex flex-col font-sm">
+            <label className="flex flex-col w-[60%] font-sm">
               Amount
               <input
                 type="text"
                 name="amount"
-                readOnly="true"
+                readOnly={true}
                 {...register("amount", { required: true,})}
                 placeholder="MK0.00"
-                className="py-2 px-8 focus:outline-none focus:ring-2 focus:ring-orange-500 rounded-lg border-2 border-gray-200 bg-gray-100"
+                className="py-2 px-10 focus:outline-none focus:ring-2 focus:ring-orange-500 rounded-lg border-2 border-gray-200 bg-gray-100"
               />
             </label>
-            <button
-              type="submit"
-              className="px-10 text-bold py-2 mt-5 rounded-xl bg-black text-white hover:scale-95 tranition duration-500"
-            >
-              Pay
-            </button>
+            <Button/>
           </form>
         </div>
       </div>

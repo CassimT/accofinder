5e988c369c7e1ae8bf419c40d53ce57625fa9635
CassimT <bed-com-24-22@unima.ac.mw>
2024-11-12 20:42:29 +0200

    SignUp intergration

--- accofinder/src/componets/auth/Signup.js
+++ accofinder/src/componets/auth/Signup.js
@@ -16,12 +16,8 @@ function Signup() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("/api/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+      const response = await fetch("http://localhost:3000/api/register", data,{
+  
       });
 
       if (response.ok) {

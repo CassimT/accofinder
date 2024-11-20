0e8eeaf6a39d4b861c03585ec8f66d5c492ca1c9
Sydney Mtima <bsc-com-22-22@unima.ac.mw>
2024-11-20 06:25:27 +0200

    Added responsiveness to footer component

--- accofinder/src/componets/footer/Footer.jsx
+++ accofinder/src/componets/footer/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"; 
 
 const Footer = () => {
   return (
     <div className="px-5 bg-slate-200">
-      <div className="flex flex-row items-center px-10 py-8  justify-between bottom-0 inset-x-0">
-        <p className="flex font-semibold">
-          &copy; 2024 AccoFinder| All Rights Reserved
+      <div className="flex flex-col sm:flex-row items-center px-10 py-8 justify-between bottom-0 inset-x-0">
+        <p className="flex font-semibold text-center sm:text-left mb-4 sm:mb-0">
+          &copy; 2024 AccoFinder | All Rights Reserved
         </p>
-        <ul className="flex flex-row gap-5">
+        <ul className="flex flex-col sm:flex-row gap-5 sm:gap-10 justify-center sm:justify-start">
           <li>
             <a href="" className="font-sans font-medium hover:text-orange-500 duration-500 hover:underline">
               Home
@@ -33,5 +33,5 @@ const Footer = () => {
     </div>
   );
 };
-export default Footer;
 
+export default Footer;

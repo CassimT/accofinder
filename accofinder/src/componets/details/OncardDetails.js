4c6a1332795b2fcf7424ce46a77654becad2a013
Tusha-15-22 <bsc-inf-15-22@unima.ac.mw>
2024-11-20 10:30:56 +0200

    added responsiveness to gallery and oncardDetails

--- accofinder/src/componets/details/OncardDetails.js
+++ accofinder/src/componets/details/OncardDetails.js
@@ -15,91 +15,103 @@ export default function OncardDetails() {
     const {Room} = location.state
 
   return (
-    <div className="  grid grid-cols-3 justify-center items-center gap-8 p-4">
-       
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-8 p-4">
     <DetailsCard 
-     icon={
-        <DollarOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-            title={"price"}
-            value={Room.price}
-             /> 
+        icon={
+            <DollarOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"price"}
+        value={Room.price}
+    /> 
 
-     <DetailsCard 
-     icon={
-        <EnvironmentOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-            title={"location"}
-            value={Room.location}
-             />
+    <DetailsCard 
+        icon={
+            <EnvironmentOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"location"}
+        value={Room.location}
+    />
 
-     <DetailsCard 
-     icon={
-        <StarOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-          title={"rating"}
-          value={Room.rating}
-             />
+    <DetailsCard 
+        icon={
+            <StarOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"rating"}
+        value={Room.rating}
+    />
 
-     <DetailsCard 
-     icon={
-        <CompassOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-          title={"Distance"}
-          value={Room.distance}
-             />
-     <DetailsCard 
-     icon={
-        <HomeOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-          title={"roomtype"}
-          value={Room.type}
-             />
-     <DetailsCard 
-     icon={
-        <CheckCircleOutlined
-          style={{
-            color: "green",
-            backgroundColor: "rgba(0,255,0,0.25)",
-            borderRadius: 20,
-            fontSize: 24,
-            padding: 8,
-          }}/>}
-          title={"Room Status"}
-          value={Room.status}
-             />
-         
+    <DetailsCard 
+        icon={
+            <CompassOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"Distance"}
+        value={Room.distance}
+    />
 
-    </div>
+    <DetailsCard 
+        icon={
+            <HomeOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"roomtype"}
+        value={Room.type}
+    />
+
+    <DetailsCard 
+        icon={
+            <CheckCircleOutlined
+                style={{
+                    color: "green",
+                    backgroundColor: "rgba(0,255,0,0.25)",
+                    borderRadius: 20,
+                    fontSize: 24,
+                    padding: 8,
+                }}
+            />
+        }
+        title={"Room Status"}
+        value={Room.status}
+    />
+</div>
+
   )
 }
